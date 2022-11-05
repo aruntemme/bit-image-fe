a83274e51332d1817597417e6e9705c74a7aa27b
aruntemme <arungauthamk@gmail.com>
2022-11-05 15:36:51 +0530

    fix

--- index.js
+++ index.js
@@ -11,23 +11,13 @@ function alertMe() {
 $(document).ready(function () {
   $('#addImage').click(function () {
     var imageUrl = $('#imageUrl').val();
-    var cardHtml = `<div class="card mt-5 ml-5" style="width: 18rem">
-    <img
-      src="${imageUrl}"
-      class="card-img-top"
-      alt="..." />
-    <div
-      class="card-body d-flex justify-content-between align-items-center">
-      <a
-        class="card-title"
-        download="image.jpg"
-        href="${imageUrl}"
-        >Download</a
-      >
-      <button class="btn btn-primary">👍 0</button>
-    </div>
-  </div>`;
-    $('#imageContainer').append(cardHtml);
+    $.ajax({
+      url: 'http://localhost:5000/images/add?image=' + imageUrl,
+      type: 'GET',
+      success: function (data) {
+        window.location.href = 'http://localhost:5500';
+      },
+    });
   });
 
   //   add like for each cards separately
